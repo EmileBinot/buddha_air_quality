ead1032b3beedf6b6370cfd4e1c7e078769c80ef
emile <emile.binot2@gmail.com>
2021-06-28 23:55:43 +0200

    simplified code, no more wifi connection, CCS811 is now calibrated by BME280 readings

--- src/main.cpp
+++ src/main.cpp
@@ -3,192 +3,161 @@
 // https://github.com/sparkfun/SparkFun_CCS811_Arduino_Library
 #include <Arduino.h>
 #include <Wire.h>
-#include <Ticker.h>
-#include <ESP8266WiFi.h>
-
 #include <SparkFunBME280.h>
 #include <SparkFunCCS811.h>
 
+#define CCS811_ADDR 0x5A  //Default I2C Address
+//#define CCS811_ADDR 0x5B //Alternate I2C Address
+#define BME280_ADDR 0x76  //Default I2C Address
+//#define BME280_ADDR 0x77 //Alternate I2C Address
+
 #define PIN_CCS811_SDA D6
 #define PIN_CCS811_SCL D7
-#define PIN_CCS811_WAK D8
 
-#define LOG_PERIOD_SEC 10
-#define ENV_PERIOD_SEC 10
-#define SHORT_PERIOD_SEC 10
-#define LONG_PERIOD_SEC 600
+//Global sensor objects
+CCS811 myCCS811(CCS811_ADDR);
+BME280 myBME280;
 
-// graph will show last ten minutes, one sample every 10 seconds
-#define TEN_MINUTES 60
-// graph will show last 24 hours, one sample every ten minutes
-#define ONE_DAY 288
+//void printDriverError( CCS811Core::status errorCode );
+void printData();
 
-static CCS811 ccs811( 0x5A );
-static BME280 bme280;
 
-#include "data.h"
-
-void HardwareSetup();
-void WifiSetup();
-void collectData();
-void WiFiCheck();
-
-typedef struct {
-  uint32_t total;
-  uint16_t count;
-} averager_t;
-
-static void print(const char* fmt, ...) {
-  char buf[256];
-  va_list args;
-  va_start( args, fmt );
-  vsnprintf( buf, sizeof(buf), fmt, args );
-  va_end( args );
-  Serial.write( buf );
-}
-
-// SETUP
-
-void setup() {
-  HardwareSetup();
-  WifiSetup();
-  collectData();
-}
-
-void HardwareSetup() {
-  // setup the pins
-  pinMode( PIN_CCS811_WAK, OUTPUT );
-  digitalWrite( PIN_CCS811_WAK, 1 );
-
-  // Enable serial
+void setup()
+{
   Serial.begin(9600);
-  print("\nCO2, TVOC, temperature, humidity sensor\n");
+  Serial.println();
+  Serial.println("Apply BME280 data to CCS811 for compensation.");
 
   // Enable I2C
   Wire.begin( PIN_CCS811_SDA, PIN_CCS811_SCL );
 
-  // Initialize BME280
-  bme280.setI2CAddress(0x76);
-  if( bme280.beginI2C() == false ) {
-    print("Failed to initialize BME280\n");
-    while(1);
-  } else {
-    print("Found BME280!\n");
+  //This begins the CCS811 sensor and prints error status of .begin()
+  /*CCS811Core::status returnCode = */myCCS811.begin();
+ /* if (returnCode != CCS811Core::SENSOR_SUCCESS)
+  {
+    Serial.println("Problem with CCS811");
+    printDriverError(returnCode);
   }
+  else
+  {*/
+    Serial.println("CCS811 online");
+  //}
 
-  // Initialize CCS811
-  digitalWrite( PIN_CCS811_WAK, 0 );
- /*CCS811Core::status returnCode =*/ ccs811.begin();
- /* if( returnCode != CCS811Core::SENSOR_SUCCESS ) {
-    print("Failed to initialize CCS811\n");
-    while(1);
-  } else {
-    print("Found CCS811!\n");
-  }*/
+  //Initialize BME280
+  //For I2C, enable the following and disable the SPI section
+  myBME280.settings.commInterface = I2C_MODE;
+  myBME280.settings.I2CAddress = BME280_ADDR;
+  myBME280.settings.runMode = 3; //Normal mode
+  myBME280.settings.tStandby = 0;
+  myBME280.settings.filter = 4;
+  myBME280.settings.tempOverSample = 5;
+  myBME280.settings.pressOverSample = 5;
+  myBME280.settings.humidOverSample = 5;
+
+  //Calling .begin() causes the settings to be loaded
+  delay(10);  //Make sure sensor had enough time to turn on. BME280 requires 2ms to start up.
+ /* byte id = */myBME280.begin(); //Returns ID of 0x60 if successful
+ /* if (id != 0x60)
+  {
+    Serial.println("Problem with BME280");
+  }
+  else
+  {*/
+    Serial.println("BME280 online");
+  //}
 }
 
-void WifiSetup() {
-  delay(5);
-  
-  // Connect to WiFi network
+void printData()
+{
+  Serial.print(" CO2[");
+  Serial.print(myCCS811.getCO2());
+  Serial.print("]ppm");
+
+  Serial.print(" TVOC[");
+  Serial.print(myCCS811.getTVOC());
+  Serial.print("]ppb");
+
+  Serial.print(" temp[");
+  Serial.print(myBME280.readTempC(), 1);
+  Serial.print("]C");
+
+  //Serial.print(" temp[");
+  //Serial.print(myBME280.readTempF(), 1);
+  //Serial.print("]F");
+
+  Serial.print(" pressure[");
+  Serial.print(myBME280.readFloatPressure(), 2);
+  Serial.print("]Pa");
+
+  //Serial.print(" pressure[");
+  //Serial.print((myBME280.readFloatPressure() * 0.0002953), 2);
+  //Serial.print("]InHg");
+
+  //Serial.print("altitude[");
+  //Serial.print(myBME280.readFloatAltitudeMeters(), 2);
+  //Serial.print("]m");
+
+  //Serial.print("altitude[");
+  //Serial.print(myBME280.readFloatAltitudeFeet(), 2);
+  //Serial.print("]ft");
+
+  Serial.print(" humidity[");
+  Serial.print(myBME280.readFloatHumidity(), 0);
+  Serial.print("]%");
+
   Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
- 
-  WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
 }
+/*
+void printDriverError( CCS811Core::status errorCode )
+{
+  switch ( errorCode )
+  {
+    case CCS811Core::SENSOR_SUCCESS:
+      Serial.print("SUCCESS");
+      break;
+    case CCS811Core::SENSOR_ID_ERROR:
+      Serial.print("ID_ERROR");
+      break;
+    case CCS811Core::SENSOR_I2C_ERROR:
+      Serial.print("I2C_ERROR");
+      break;
+    case CCS811Core::SENSOR_INTERNAL_ERROR:
+      Serial.print("INTERNAL_ERROR");
+      break;
+    case CCS811Core::SENSOR_GENERIC_ERROR:
+      Serial.print("GENERIC_ERROR");
+      break;
+    default:
+      Serial.print("Unspecified error.");
+  }
+*/
 
-void collectData() {
-  static averager_t tvoc_avg = {0, 0};
-  static averager_t co2_avg = {0, 0};
-  static unsigned long second_log = 0;
-  static unsigned long second_env = 0;
-  static unsigned long second_short = 0;
-  static unsigned long second_long = 0;
-  static uint16_t tvoc = 0;
-  static uint16_t co2 = 0;
-  
-  if( ccs811.dataAvailable() ) {
-    ccs811.readAlgorithmResults();
-    uint16_t tvoc = ccs811.getTVOC();
-    uint16_t co2 = ccs811.getCO2();
-    tvoc_avg.total += tvoc;
-    co2_avg.total += co2;
-    tvoc_avg.count++;
-    co2_avg.count++;
+void loop()
+{
+  //Check to see if data is available
+  if (myCCS811.dataAvailable())
+  {
+    //Calling this function updates the global tVOC and eCO2 variables
+    myCCS811.readAlgorithmResults();
+    //printData fetches the values of tVOC and eCO2
+    printData();
+
+    float BMEtempC = myBME280.readTempC();
+    float BMEhumid = myBME280.readFloatHumidity();
+
+    Serial.print("Applying new values (deg C, %): ");
+    Serial.print(BMEtempC);
+    Serial.print(",");
+    Serial.println(BMEhumid);
+    Serial.println();
+
+    //This sends the temperature data to the CCS811
+    myCCS811.setEnvironmentalData(BMEhumid, BMEtempC);
+  }
+  else if (myCCS811.checkForStatusError())
+  {
+    Serial.println(myCCS811.getErrorRegister()); //Prints whatever CSS811 error flags are detected
   }
 
-  unsigned long second = millis() / 1000;
-  if( (second - second_log) > LOG_PERIOD_SEC ) {
-    second_log = second;
-
-    if( tvoc_avg.count > 0 ) {
-      tvoc = tvoc_avg.total / tvoc_avg.count;
-      tvoc_avg.total = 0;
-      tvoc_avg.count = 0;
-      print("TVOC: %u\n", tvoc);
-    }
-    if( co2_avg.count > 0 ) {
-      co2 = co2_avg.total / co2_avg.count;
-      co2_avg.total = 0;
-      co2_avg.count = 0;
-      print("CO2: %u\n", co2);
-    }
-  }
-  
-  if( (second - second_short) > SHORT_PERIOD_SEC ) {
-    second_short = second;
-    tenMinutesTVOC[ tenMinuteCounter ] = tvoc;
-    tenMinutesCO2[ tenMinuteCounter ] = co2;
-    tenMinuteCounter = ++tenMinuteCounter % TEN_MINUTES;
-  }
-
-  if( (second - second_long) > LONG_PERIOD_SEC ) {
-    second_long = second;
-    oneDayTVOC[ oneDayCounter ] = tvoc;
-    oneDayCO2[ oneDayCounter ] = co2;
-    oneDayCounter = ++oneDayCounter % ONE_DAY;
-  }
-
-  if( (second - second_env ) > ENV_PERIOD_SEC ) {
-    second_env = second;
-
-    digitalWrite(PIN_CCS811_WAK, 1);
-    tempC = bme280.readTempC();
-    humidity = bme280.readFloatHumidity();
-    digitalWrite(PIN_CCS811_WAK, 0);
-    print("Temp: %.2fºC Humidity: %.2f%%\n", tempC, humidity);
-    ccs811.setEnvironmentalData(tempC, humidity);
-  }  
-}
-
-// LOOP
-
-void loop() {
-  WiFiCheck();  
-  collectData();
-}
-
-void WiFiCheck() {
-  int wifi_retry = 0;
-  while(WiFi.status() != WL_CONNECTED && wifi_retry < 5 ) {
-      wifi_retry++;
-      Serial.println("WiFi not connected. Try to reconnect");
-      WiFi.disconnect();
-      WiFi.mode(WIFI_OFF);
-      WiFi.mode(WIFI_STA);
-      WiFi.begin(ssid, password);
-      delay(100);
-  }
-  if(wifi_retry >= 5) {
-      Serial.println("\nReboot");
-      ESP.restart();
-  }
+  delay(2000); //Wait for next reading
 }
